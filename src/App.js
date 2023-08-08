b13d435becac276b8ae0b33b7663ad137ddc586a
ChromOnAndSlam <S3947728@student.rmit.edu.au>
2023-08-08 16:20:56 +1000

    Added Structure to Program
    - added logo
    - added header, main, footer and nav bar
    - added some css to style theheader
    - suggested colour palette

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import logo from './movie.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="webHeader">
+        <h1 id="brandName">Loop Cinemas</h1>
+        <img src={logo} alt="logo"></img>
       </header>
+      <nav>
+        <i>test</i>
+      </nav>
+      <main>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sodales tincidunt magna, nec hendrerit lectus congue et. Quisque nisi felis, lobortis ac mauris vel, pulvinar malesuada erat. Ut pharetra in nisl eget ornare. Pellentesque faucibus at nulla tristique mattis. Sed imperdiet dui ut nulla volutpat accumsan. Fusce luctus, lorem eu feugiat consectetur, tellus purus euismod erat, ut faucibus ligula neque id mi. Vestibulum dignissim nulla eget metus viverra consequat. Nullam ac metus vel ex consequat ullamcorper. Cras in metus diam. In maximus id odio vitae rhoncus. In sed cursus lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc elementum eget metus euismod accumsan.</p>
+      </main>
+      <footer>
+        <p>something</p>
+      </footer>
     </div>
   );
 }
