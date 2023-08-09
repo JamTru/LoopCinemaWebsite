afd3d4baabc29963af5a9e744900b00bea2f4d88
ChromOnAndSlam <S3947728@student.rmit.edu.au>
2023-08-09 18:50:03 +1000

    Logo Adjustment
    - Adjusted CSS of logo so smaller padding and logo is in line

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import logo from '../movie.png'
 const Header = () => {
   return (
     <div class="webHeader">
+      <img src={logo} alt="logo" class="mainLogo"></img>
       <h1 id="brandName">Loop Cinemas</h1>
-        <img src={logo} alt="logo" class="mainLogo"></img>
     </div>
   );
 };
-  
+
 export default Header;
