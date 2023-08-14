b76dbc62c55b2df83894aa67ddfa2ac213743025
Jinwon_Yang <s3888752@student.rmit.edu.au>
2023-08-14 23:42:37 +1000

    Added Button on the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import logo from '../movie.png'
+import { Link } from "react-router-dom"
+
 const Header = () => {
+
   return (
     <div class="webHeader">
       <img src={logo} alt="logo" class="mainLogo"></img>
       <h1 id="brandName">Loop Cinemas</h1>
+      <button type='button' className='login_button'>
+        <Link to='../LoginForm.js' >
+          Login
+        </Link>
+      </button>
     </div>
   );
 };
