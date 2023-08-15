546d97675ae3c6e8284f8b30774521db9c165d53
Jinwon_Yang <s3888752@student.rmit.edu.au>
2023-08-15 14:42:59 +1000

    (Need to fix) Added Modal on Sign Up botton
    Modal loads on the main page, not as a modal saparately

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { useState } from "react"
 import logo from '../movie.png'
-import { Link } from "react-router-dom"
+import SignUpModal from "../modals/SignUpModal";
+import {Modal, Button} from 'react-bootstrap'
 
 const Header = () => {
+  const [signUpModalOn, setSignUpModalOn] = useState(false);
 
   return (
-    <div class="webHeader">
-      <img src={logo} alt="logo" class="mainLogo"></img>
-      <h1 id="brandName">Loop Cinemas</h1>
-      <button type='button' className='login_button'>
-        <Link to='../LoginForm.js' >
-          Login
-        </Link>
-      </button>
-    </div>
+    <>
+      <SignUpModal 
+        show={signUpModalOn}
+        onHide={ ()=> setSignUpModalOn(false)} 
+      />
+      {/* show prop allows user to see the modal */}
+      {/* onHide is to hide the modal */}
+    
+      <div class="webHeader">
+        <img src={logo} alt="logo" class="mainLogo"></img>
+        <h1 id="brandName">Loop Cinemas</h1>
+        
+        <button type="button"
+        className='signUp_button' onClick={ ()=> setSignUpModalOn(true)}>
+          Sign Up
+        </button>
+        
+        <button type="button" className='signIn_button' onClick={ ()=> {}}>
+          Sign In
+        </button>
+      </div>
+    </>
   );
 };
 
