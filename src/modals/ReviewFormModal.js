3bbfbb539f728afe84c5dc5287606330463c121d
ChromOnAndSlam <S3947728@student.rmit.edu.au>
2023-08-21 22:21:03 +1000

    Properly Handled Submission Validates for empty inputs and comments over 250 characters before closing modal

--- src/modals/ReviewFormModal.js
+++ src/modals/ReviewFormModal.js
@@ -5,8 +5,20 @@ import { Modal, Container } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 
 const ReviewFormModal = (props) => {
-  const handleSubmit = () => {
-    console.log("Form submitted");
+  const [numRating, setNumRating] = useState(0);
+  const [comments, setComments] = useState("");
+  const [errorDetected, setErrorDetected] = useState(null);
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(comments.length);
+    if (comments.length <= 0 || comments.length > 250) {
+      setErrorDetected("Comments need to be between 0 to 250 characters.")
+      console.log("Error message");
+    }
+    else {
+      console.log("Successful Submission");
+      props.onHide();
+    }
   }
   return (
     <Modal show={props.show} onHide={props.onHide}>
@@ -18,16 +30,21 @@ const ReviewFormModal = (props) => {
         <form onSubmit={handleSubmit} id="reviewForm">
           <div className="form-group">
             <label htmlFor="numRating" className="control-label">Rate from 0 to 100</label>
-            <input type="number" name="numRating" id="numRating" min="0" max="100" className="form-control" required></input>
+            <input type="number" name="numRating" id="numRating" min="0" max="100" className="form-control" required onChange={e => setNumRating(e.target.value)}></input>
           </div>
+          <div className="form-group">
+            <label htmlFor="comments" className="control-label">What were your thoughts?</label>
+            <textarea rows="4" cols="50" name="comments" form="reviewForm" onChange={e => setComments(e.target.value)} required></textarea>
+          </div>
+          <Button type="submit" variant="primary">Submit</Button>
         </form>
-        <div className="form-group">
-          <label htmlFor="comments" className="control-label">What were your thoughts?</label>
-          <textarea rows="4" cols="50" name="comments" form="reviewForm"></textarea> 
-        </div>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="primary" onClick={props.onHide}>Submit</Button>
+        {errorDetected != null &&
+          <div className="form-group">
+            <span className="text-danger">{errorDetected}</span>
+          </div>
+        }
       </Modal.Footer>
     </Modal>
   )
