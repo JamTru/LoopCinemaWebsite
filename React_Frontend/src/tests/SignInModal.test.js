c0372f13df388d7edb7312aee0a128b4edd91a52
ChromOnAndSlam <S3947728@student.rmit.edu.au>
2023-10-04 16:38:26 +1100

    Attempted to Add Testing Modules Wrote Test Classes Will need to solve issue

---
+++ React_Frontend/src/tests/SignInModal.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, screen, fireEvent } from "@testing-library/react";
+import SignInModal from '../modals/SignInModal.js';
+
+let container;
+let utils;
+
+//Before each test, render the component in a test environment.
+beforeEach(() =>{
+  function dummyFunction() {return};
+  utils = render(
+    <ReviewFormModal show={true} />
+  );
+  container = utils.container;
+});
+
+//Tests to see whether the component itself renders properly within the HTML Document
+test("Renders Sign Up Form Properly", () => {
+  expect(container).toBeInTheDocument();
+});
